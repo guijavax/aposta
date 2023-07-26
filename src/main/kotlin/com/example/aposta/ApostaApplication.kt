62c7b814cdc95f0d433d19f27710e83ef86f5433
guilhermealves <guilhermeborgesti@gmail.com>
2023-07-26 20:02:41 -0300

    first commit

---
+++ src/main/kotlin/com/example/aposta/ApostaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aposta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApostaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApostaApplication>(*args)
+}
