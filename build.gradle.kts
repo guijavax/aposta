fdbc70d239d46a7ed76b9936b674e3384d0433f0
guilhermealves <guilhermeborgesti@gmail.com>
2023-08-12 19:33:20 -0300

    mudanca na estrutura do build

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+
+plugins {
+	val versionDefault = "1.8.22"
+
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("jvm") version versionDefault
+	kotlin("plugin.spring") version versionDefault
+	kotlin("plugin.jpa") version versionDefault
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation ("org.springframework.boot:spring-boot-starter-web")
+	implementation ("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation ("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("com.h2database:h2:2.2.220")
+
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
