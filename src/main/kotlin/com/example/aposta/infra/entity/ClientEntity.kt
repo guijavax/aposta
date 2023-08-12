4e446dc7927d4a78c6e9e1772e60a60fa5fd4ae0
guilhermealves <guilhermeborgesti@gmail.com>
2023-08-12 19:47:57 -0300

    adicao do h2

--- src/main/kotlin/com/example/aposta/infra/entity/ClientEntity.kt
+++ src/main/kotlin/com/example/aposta/infra/entity/ClientEntity.kt
@@ -1,13 +1,16 @@
 package com.example.aposta.infra.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 
 @Entity
 @Table(name = "client")
 data class ClientEntity(
-    val id : Int,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+
+    val id : Long,
     val nome : String,
     val cpf : Long,
     val idade : Int,
