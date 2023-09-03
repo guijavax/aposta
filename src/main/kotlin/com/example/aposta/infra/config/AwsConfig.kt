6a4fc1b8a06fc9956bcfad22723e9df33cee76d5
guilhermealves <guilhermeborgesti@gmail.com>
2023-09-03 18:17:01 -0300

    adicao de config do s3

--- src/main/kotlin/com/example/aposta/infra/config/AwsConfig.kt
+++ src/main/kotlin/com/example/aposta/infra/config/AwsConfig.kt
@@ -4,6 +4,8 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,12 +16,21 @@ class AwsConfig {
     @Value(value = "\${aws.region}")
     lateinit var regionAws: String
 
+    var credentialsAws : AWSStaticCredentialsProvider = AWSStaticCredentialsProvider(EnvironmentVariableCredentialsProvider().credentials)
+
     @Bean
     fun getIdentityProvider(): AWSCognitoIdentityProvider =
         AWSCognitoIdentityProviderClientBuilder
             .standard().apply {
-                credentials = AWSStaticCredentialsProvider(EnvironmentVariableCredentialsProvider().credentials)
+                credentials = credentialsAws
                 region = regionAws
             }.build()
+
+    @Bean
+    fun s3Config()  =
+        AmazonS3ClientBuilder.standard().apply {
+            credentials = credentialsAws
+            region = regionAws
+        }.build()
 
 }
